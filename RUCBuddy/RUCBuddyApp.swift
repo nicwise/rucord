18c01dabc60ec355fb379f51f2d7224edc3d1c5c
Nic Wise <nic.wise@tend.nz>
2025-08-09 16:00:35 +1200

    Initial Commit

---
+++ RUCBuddy/RUCBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RUCBuddyApp.swift
+//  RUCBuddy
+//
+//  Created by Nic Wise on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct RUCBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
