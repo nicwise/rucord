09b50718fc05d1d77bdd9cbba27508cd10e89a73
Nic Wise <nic.wise@tend.nz>
2025-08-29 16:05:57 +1200

    Set accent color to #4ab1ff

--- Rucord/RucordApp.swift
+++ Rucord/RucordApp.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+extension Color {
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
+
 @main
 struct RucordApp: App {
     @StateObject private var store = CarStore()
@@ -15,6 +27,7 @@ struct RucordApp: App {
         WindowGroup {
             CarListView()
                 .environmentObject(store)
+                .tint(Color(hex: 0x4ab1ff))
         }
     }
 }
